4bd0e5cd11912f9635db05b41a707f1ef4b387d7
githarshil <harshil.456.sharma@gmail.com>
2026-06-25 12:04:42 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,50 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product";
-import HomePage from "./pages/HomePage";
-import Pricing from "./pages/Pricing";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
+import { lazy, Suspense } from "react";
 import CityList from "./components/CityList";
 import CountriesList from "./components/CountriesList";
 import City from "./components/City";
 import Form from "./components/Form";
-import PageNotfound from "./pages/PageNotFound";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
-// import City from "./components/City";
-// import Countryitem from "./components/CountryItem";
-// import NavBar from "./components/NavBar";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+const HomePage = lazy(() => import("./pages/Homepage"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const Login = lazy(() => import("./pages/Login"));
+const Product = lazy(() => import("./pages/Product"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 
 function App() {
-  // const [cities, setCities] = useState([]);
-  // const [loading, setLoading] = useState(false);
-  // useEffect(() => {
-  //   async function loadCities() {
-  //     try {
-  //       setLoading(true);
-  //       const res = await fetch(`${BASE_URL}`);
-  //       const data = await res.json();
-  //       console.log(data);
-  //       setCities(data);
-  //     } catch (err) {
-  //       console.error("Error fetching cities:", err);
-  //       alert("There was an error fetching the cities");
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-  //   loadCities();
-  // }, []);
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<HomePage />} />
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<HomePage />} />
 
-            <Route path="/product" element={<Product />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/login" element={<Login />} />
+              <Route path="/product" element={<Product />} />
+              <Route path="/pricing" element={<Pricing />} />
+              <Route path="/login" element={<Login />} />
 
-            <Route
-              path="/app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountriesList />} />
-              <Route path="form" element={<Form />}></Route>
-            </Route>
-            <Route path="*" element={<PageNotfound />}></Route>
-          </Routes>
+              <Route
+                path="/app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountriesList />} />
+                <Route path="form" element={<Form />}></Route>
+              </Route>
+              <Route path="*" element={<PageNotFound />}></Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
